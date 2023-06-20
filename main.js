983303b761f4c7e8b3ce2b2dcde71c8415752c7d
halg830 <halg830@gmail.com>
2023-06-19 21:18:23 -0500

    ajuste diseño

--- main.js
+++ main.js
@@ -5,29 +5,16 @@ const proyects = [
   "./Factura/index.html",
   "./Galería/index.html",
   "./Pagina_quien_soy/index.html",
-  "./Semaforo/index.html",
-  "./Calculadora/index.html",
-  "./Calendario/index.html",
-  "./Curriculum/cv.html",
-  "./Factura/index.html",
-  "./Galería/index.html",
-  "./Pagina_quien_soy/index.html",
-  "./Semaforo/index.html",
-  "./Calculadora/index.html",
-  "./Calendario/index.html",
-  "./Curriculum/cv.html",
-  "./Factura/index.html",
-  "./Galería/index.html",
-  "./Pagina_quien_soy/index.html",
   "./Semaforo/index.html"
 ];
 const fondos = [
-  "https://img.freepik.com/iconos-gratis/calculadora_318-798605.jpg",
+  "./img/calculadora.png",
   "https://cdn.icon-icons.com/icons2/3150/PNG/512/calendar_icon_192645.png",
-  "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921",
-  "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921",
-  "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921",
-  "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921",
+  "./img/cv.png",
+  "./img/factura.png",
+  "./img/galeria.png",
+  "./img/quienSoy.png",
+  "./img/semaforo.png"
 ];
 
 const extra = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"
